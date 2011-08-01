e5000444552e8cfd874e73058308bc2e7da8b147
S.Nishio <nishio@densan-labs.net>
2011-08-01 13:00:31 +0900

    change setting filename from application.yml to sphinx_plugin_setting.yml

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,6 +1,6 @@
 require 'settingslogic'
 
 class Settings < Settingslogic
-  source "#{Rails.root}/config/application.yml"
+  source "#{Rails.root}/config/sphinx_plugin_setting.yml"
   namespace Rails.env
 end
